878bfec3fe346573fe8ed11ce28aecffc852cb3e
nadiaamali4 <541231151@student.smktelkom-pwt.sch.id>
2024-10-22 15:45:03 +0700

    add landingpage

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\DepanController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +20,4 @@ Route::get('/', function () {
     return 'Hello World';
 });
 
-Route::get('/depan', [DepanController::class, 'index']);
+Route::get('/landingpage', [TaskController::class, 'index']);
